56f18c00d12a7fd63ac7e3cb3ab7bb74e045ab62
Bacha Soufiane <gl.soufianebacha@gmail.com>
2022-04-17 01:40:08 +0100

    add file to local repo

---
+++ js/Data_type.py
@@ -0,0 +1,11 @@
+
+# type() is a function that ca use to know the data type of variables
+print(type(10)) #data type =>Int:Integer
+print(type(-10)) #data type =>Int:Integer
+print(type(10.10)) #data type =>Float:Float
+print(type(-10.10)) #data type =>Float:Float
+print(type("I love python")) #data type =>Str:String
+print(type([1,2,3,4,5])) #data type =>List
+print(type((1,2,3,4,5))) #data type =>tuple
+print(type({"A":1,"B":2,"C":3})) #data type =>Dic:dictionary
+print(type(2==2)) #data type =>bool:boolean
---
+++ php/variable.py
@@ -0,0 +1,14 @@
+
+#snake case assigenmnet
+my_name="bacha"
+print(my_name)
+#camel case assigenmnet
+myName="bacha soufiane"
+print(myName)
+#normal case assigenmnet
+myname="bacha"
+print(myname)
+#multiple assigenmnet varaible
+help("keywords")
+A,B,C=1,2,3
+print(C)
